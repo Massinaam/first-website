68ea547a7d17491b74dc5bcfd2fb17f045a6998d
Massinaam <77203237+Massinaam@users.noreply.github.com>
2026-01-30 17:56:37 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <title>Accueil</title>
+</head>
+<body>
+
+<div class="wrapper">
+    <nav class="nav">
+    <div class="nav-logo">
+        <img src="https://www.imt-atlantique.fr/sites/default/files/Images/Ecole/charte-graphique/IMT_Atlantique_logo_RVB_Baseline_400x272.jpg" alt="Logo" style="width: 50%;">
+    </div>
+        <div class="nav-menu" id="navMenu">
+            <ul>
+                <li><a href="#" class="link active">Accueil</a></li>
+                <li><a href="#" class="link">Prendre rendez-vous</a></li>
+                <li><a href="#" class="link">Encadrantes</a></li>
+                <li><a href="#" class="link">À propos</a></li>
+            </ul>
+        </div>
+        <div class="nav-menu-btn">
+            <i class="bx bx-menu" onclick="myMenuFunction()"></i>
+        </div>
+    </nav>
+    <div class="form-box">
+        <!------------------- accueil form -------------------------->
+
+        <div class="connexion-container" id="connexion">
+            <div class="top">
+                <header>Bienvenue sur la plateforme de prise de rendez-vous pour votre accompagnement professionnel</header>
+            </div>
+
+        </div>
+</div>
+
+</body>
+</html>
